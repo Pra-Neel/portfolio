2bc0686b8ae97ff92575f6a2a93becbf240c2a2d
manita <manitamaharjan055@gmail.com>
2023-05-08 19:58:16 +0545

    registration added

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -3,6 +3,7 @@ from django.urls import path #here includes is removed from project urls
 from custom_auth import views #this imports views from custom_auth apps
 
 urlpatterns = [
-    path('login/', views.login, name = 'login' ) #comment ??
+    path('login/', views.login, name = 'login' ), #comment ??
+    path('signup/', views.signup, name = 'signup')
     
 ]
--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import request
+from django.http import HttpResponse
 from django.contrib.auth.models import User
-import custom_auth
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -9,8 +9,35 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def login(request):
-    if request.method == 'GET':
+    if request.method == 'POST': 
+        username = request.POST[username]
+        password = request.POST[password]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user )
+            return redirect('/home')
+        else:
+            return HttpResponse('Login error')
+    else:
         return render(request, 'login.html')
+    
+    #what is difference between render and redirect??
 
+
+def signup(request):
+    if request.method == 'POST':
+        # fullname = request.POST['fullname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+    
+        if password == confirm_password:
+            user = User.objects.create_user( username=username, email=email, password=password)
+            return redirect('/login')
+        else:
+            return HttpResponse('password not matched.')
+        
+    return render(request, 'signup.html')
 
 
